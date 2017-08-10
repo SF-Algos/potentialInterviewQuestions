496f67cdea0e75fd89708aa12925ebfba1d674bc
Gina Benavidez <ginabenavidez@Ginas-MacBook-Pro.local>
2017-08-10 12:35:22 -0700

    remove string question and add question: 'findMostFrequent()' which finds the element in an array which occurs the most

--- Arrays/writtenInJavascript.js
+++ Arrays/writtenInJavascript.js
@@ -5,12 +5,34 @@ function mergeTwoArrays(arr){
 
 mergeTwoArrays([ ['b', 'a'], ['l', 'z', 'y'] ]);
 
-// - Find out if a given string is a palindrome
-function palindrome(str){
-  if (str.toLowerCase() === str.toString().toLowerCase().split('').reverse().join('')){
-    return true;
-  } else {
-    return false;
+// given an array of integers, find the number that is repeated the most.
+function findMostFrequent(arr){
+  var repsCount = {};
+  for(i=0; i<arr.length; i++){
+    if(!repsCount[arr[i]]){
+      repsCount[arr[i]] = 1;
+    } else {
+      repsCount[arr[i]] = repsCount[arr[i]] + 1;
+    }
   }
+  var occursMore = [];
+  for (var key in repsCount){
+    if (repsCount[key] > occursMore){
+      occursMore.push(key);
+    }
+  }
+  return occursMore;
 }
-  
+// I'd especially like to see other approaches to this problem, comment if you have a different approach.
+
+// - Find all even numbers in array
+function evenOnly(array){
+  var onlyEven = [];
+  for (i=0; i<array.length; i++){
+    if(array[i] % 2 === 0){
+      onlyEven.push(array[i]);
+    }
+  }
+  return onlyEven;
+}
+gina
