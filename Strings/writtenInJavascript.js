2c122d90cb33f95cac3edeba34e99d05f22c10c6
Gina Benavidez <ginabenavidez@Ginas-MacBook-Pro.local>
2017-08-10 12:12:37 -0700

    add question: 'reverse a string'

--- Strings/writtenInJavascript.js
+++ Strings/writtenInJavascript.js
@@ -6,4 +6,12 @@ function palindrome(str){
     return false;
   }
 }
-  
+
+// reverse a string.
+function reverseString(str) {
+  var reversed = [];
+  for(i = str.length - 1; i >= 0; i--){
+    reversed.push(str[i]);
+  }
+  return reversed.join("");
+}
